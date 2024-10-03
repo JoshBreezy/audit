99d7642f94f901e7421dc161a7eb82368fb6a2f1
Josh <81214015+JoshBreezy@users.noreply.github.com>
2024-10-03 00:30:31 -0700

    login modal

--- src/Components/VenturaQ3.js
+++ src/Components/VenturaQ3.js
@@ -4,7 +4,7 @@ import oldAudit from '../OldAudits/oldAudit.pdf';
 export default function VenturaQ3 () {
     return (
         <Container className='vh-100'>
-            <iframe src ={oldAudit} title='oldAudit' className='col-12' />
+            <embed src ={oldAudit} className='col-12' />
         </Container>
     )
 };
